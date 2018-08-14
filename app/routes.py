1416c4ad8d483240673ad944696a2c277060fbda
Shirish <shirish@transporter.city>
2018-08-14 17:45:44 +0530

    templating

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,20 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hi!'
+    user = {'username' : 'Shirish'}
+    
+    posts = [
+        {
+            'author': {'username': 'Mark Manson'},
+            'body': 'Avoid Entitlement!'
+        },
+        {
+            'author': {'username': 'Amish Tripathi'},
+            'body': 'The Universe bows to lord Shiva, I bow to lord Shiva!'
+        }
+    ]
+
+    return render_template('index.html', title = 'Home', user = user, posts = posts)
